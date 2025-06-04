14f2ec9e96207aca1a67a59e3e4abf2248e2d736
yektaty <yektayt10@gmail.com>
2025-06-04 12:14:31 +0330

    sign up page

---
+++ signup.cpp
@@ -0,0 +1,37 @@
+#include "signup.h"
+#include "ui_signup.h"
+
+SignUP::SignUP(MainWindow* mainW,QWidget *parent): QWidget(parent), ui(new Ui::SignUP),mainW(mainW)
+{
+    ui->setupUi(this);
+}
+
+SignUP::~SignUP()
+{
+    delete ui;
+}
+
+
+void SignUP::on_pushButtoYes_clicked()
+{
+    QString name=ui->lineEdit_name->text().trimmed();
+    QString family = ui->lineEdit_family->text().trimmed();
+    QString username = ui->lineEdit_usernem->text().trimmed();
+    QString password1 = ui->lineEdit_pass->text().trimmed();
+    QString password2=ui->lineEdit_pass_2->text().trimmed();
+    QString phone= ui->lineEdit_phon->text().trimmed();
+    if (name.isEmpty() || family.isEmpty() || username.isEmpty() || password1.isEmpty() || phone.isEmpty())
+    {
+        QMessageBox::warning(this, "خطا", "لطفاً همه‌ی فیلدها را پر کنید");
+        return;
+    }
+    if(password1!=password2)
+    {
+        QMessageBox::warning(this,"خطا", "رمز و تکرار آن یکسان نیست.لطفا دوباره امتحان کنید");
+        return;
+    }
+
+    this->hide();
+    mainW->show();
+}
+
---
+++ signup.h
@@ -0,0 +1,29 @@
+#ifndef SIGNUP_H
+#define SIGNUP_H
+#include <QWidget>
+#include <QMessageBox>
+#include "mainwindow.h"
+class MainWindow;
+
+namespace Ui {
+class SignUP;
+}
+
+class SignUP : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SignUP(MainWindow* mainW,QWidget *parent = nullptr);
+    ~SignUP();
+
+
+private slots:
+    void on_pushButtoYes_clicked();
+
+private:
+    Ui::SignUP *ui;
+    MainWindow *mainW;
+};
+
+#endif // SIGNUP_H
