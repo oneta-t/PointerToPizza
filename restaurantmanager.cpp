070569b64cefb97ba8b030b5d0c4fc28129b1b41
yektaty <yektayt10@gmail.com>
2025-06-27 14:25:51 +0330

    برامی مدیر رستوران

---
+++ restaurantmanager.cpp
@@ -0,0 +1,16 @@
+#include "restaurantmanager.h"
+#include "ui_restaurantmanager.h"
+
+RestaurantManager::RestaurantManager(QWidget *parent): QWidget(parent), ui(new Ui::RestaurantManager)
+{
+    ui->setupUi(this);
+}
+
+RestaurantManager::~RestaurantManager()
+{
+    delete ui;
+}
+
+int RestaurantManager::NextId=0;
+
+RestaurantManager::RestaurantManager(QString name,QString family,QString passw,QString username,QString phone,QString role):User(name,family,passw,username,phone,role){}
---
+++ restaurantmanager.h
@@ -0,0 +1,27 @@
+#ifndef RESTAURANTMANAGER_H
+#define RESTAURANTMANAGER_H
+
+#include <QWidget>
+#include "user.h"
+
+namespace Ui {
+class RestaurantManager;
+}
+
+class RestaurantManager : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit RestaurantManager(QWidget *parent = nullptr);
+    RestaurantManager(QString name,QString family,QString passw,QString username,QString phone,QString role);
+    int getId() const;
+    ~RestaurantManager();
+
+private:
+    Ui::RestaurantManager *ui;
+    static int NextId;
+    int ID;
+};
+
+#endif // RESTAURANTMANAGER_H
