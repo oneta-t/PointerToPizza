c554b7bc369e0ba87b77b298c3f6fed95d91907d
mrzisi <maes25071383@gmail.com>
2025-07-01 17:19:23 +0330

    Update order.cpp

--- order.cpp
+++ order.cpp
@@ -1,11 +1,29 @@
 #include "order.h"
 
-Order::Order(int id, Customer* customer, RestaurantManager* restaurant, const QStringList& items, const QString& status, DeliveryPerson* deliveryPerson)
-    : id(id), customer(customer), restaurant(restaurant), items(items), status(status), deliveryPerson(deliveryPerson)
+Order::Order(int id, Customer* customer, RestaurantManager* restaurant, const QStringList& items, Order::Status status, DeliveryPerson* deliveryPerson)
+    : id(id), customer(customer), restaurant(restaurant), items(items), status(status), deliveryPerson(deliveryPerson), statusHistory(new OrderStatusHistory)
 {
+    statusHistory->addStatusChange(getStatusText()); // ثبت وضعیت اولیه
 }
 
 Order::~Order()
 {
     delete deliveryPerson;
+    delete statusHistory; //  پاک‌سازی تاریخچه
+}
+
+void Order::setStatus(Status newStatus)
+{
+    status = newStatus;
+    statusHistory->addStatusChange(getStatusText()); //  افزودن به تاریخچه
+}
+
+QString Order::getStatusText() const
+{
+    switch (status) {
+    case Preparing: return "آماده‌سازی";
+    case Shipping: return "ارسال";
+    case Delivered: return "تحویل";
+    default: return "در حال پردازش";
+    }
 }
